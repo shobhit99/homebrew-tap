b6b0df7be282d431a5b0a699c750c3ad1667be20
shobhit99 <shobhitbhosure7@gmail.com>
2026-05-22 00:23:42 +0530

    updated sha

--- Casks/superisland.rb
+++ Casks/superisland.rb
@@ -2,12 +2,12 @@ cask "superisland" do
   version "1.0.8"
 
   on_arm do
-    sha256 "REPLACE_WITH_ARM64_DMG_SHA256"
+    sha256 "f47339e68ff815e53240ea5163388ebecbc9b0722c7be7f33c49853376d7d5ba"
     url "https://github.com/shobhit99/SuperIsland/releases/download/#{version}/SuperIsland.dmg"
   end
 
   on_intel do
-    sha256 "REPLACE_WITH_X86_64_DMG_SHA256"
+    sha256 "eb26abd006d463b853927bffb0c32f9d2b9f945092af0beed5a1dc27ba682a50"
     url "https://github.com/shobhit99/superisland/releases/download/#{version}/SuperIsland-x86_64.dmg"
   end
 
