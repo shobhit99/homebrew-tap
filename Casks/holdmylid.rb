2a0946e9dd2a828deaca8e563c82b97035524aea
Shobhit Bhosure <shobhitbhosure7@gmail.com>
2026-06-16 23:53:48 +0530

    Update holdmylid.rb

--- Casks/holdmylid.rb
+++ Casks/holdmylid.rb
@@ -1,6 +1,6 @@
 cask "holdmylid" do
-  version "1.0.0"
-  sha256 "4507d99cfb87bb7ebfb46cd535caba09dc42a99b3541c2a9b4a93ccb51ba9595"
+  version "1.0.1"
+  sha256 "b8aa7900d14cb588cb6569ad899227e55b640315c0120bdb8472c7941ffac01b"
 
   url "https://github.com/shobhit99/holdmylid-releases/releases/download/#{version}/HoldMyLid.dmg"
   name "Hold My Lid"
